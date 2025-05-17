19a65608d1bdb7258493446288e34495d7db2a40
veggiebob <veggiebobowman@gmail.com>
2025-05-17 13:54:33 -0400

    remove target from repo

--- values_macro_derive/target/debug/deps/values_macro_derive-dfb2a648abb2c2cf.d
+++
@@ -1,5 +0,0 @@
-/home/veggiebob/repos/game-42/values_macro_derive/target/debug/deps/libvalues_macro_derive-dfb2a648abb2c2cf.so: src/lib.rs
-
-/home/veggiebob/repos/game-42/values_macro_derive/target/debug/deps/values_macro_derive-dfb2a648abb2c2cf.d: src/lib.rs
-
-src/lib.rs:
--- values_macro_derive/target/debug/libvalues_macro_derive.d
+++
@@ -1,1 +0,0 @@
-/home/veggiebob/repos/game-42/values_macro_derive/target/debug/libvalues_macro_derive.so: /home/veggiebob/repos/game-42/values_macro_derive/src/lib.rs
